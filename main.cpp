91f6a1dc82af0e15b5831de28f5c3b3a2e5ef07c
nhershy <nhershy@gmail.com>
2016-07-25 12:25:01 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,30 @@
+//pring the running sum of the previous digits, starting from left
+#include <iostream>
+using namespace std;
+
+void DigiSum(int num, int mod, int sum);
+
+int main() 
+{
+	
+	//int num = 12345;
+	int num = 56372;
+	DigiSum(num, 10000, 0);
+	return 0;
+}
+
+void DigiSum(int num, int mod, int sum)
+{
+	int temp=0;
+	if (mod==0)
+		return;
+	else
+	{
+		temp = num / mod;
+		sum += temp;
+		cout << sum << " ";
+		num = num % mod;
+		mod = mod/ 10;
+		DigiSum(num, mod, sum);
+	}
+}
